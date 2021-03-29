a2365b44266405af556aa552b5f405cb492965a5
DESKTOP-GH9HNV6\qijb <jianboqi@126.com>
2021-03-29 08:55:12 +0800

    修改

---
+++ treemetricextract.R
@@ -0,0 +1,40 @@
+library(TreeLS)
+
+# open sample plot file
+# file = system.file("extdata", "pine_plot.laz", package="TreeLS")
+file = 'D:\DatasetDir\ZCZW\MobileLiDAR\dual_scanners_sub.las'
+tls = readTLS(file)
+
+# normalize the point cloud
+tls = tlsNormalize(tls, keep_ground = FALSE)
+#x = plot(tls,colorPalette="White") #
+
+# extract the tree map from a thinned point cloud
+thin = tlsSample(tls, smp.voxelize(0.04))
+map = treeMap(thin, map.hough(min_density = 0.1), 0)
+x = plot(map, size=2)
+# add_treeMap(x, map, color='yellow', size=2)
+
+# classify tree regions
+tls = treePoints(thin, map, trp.autoseg());
+add_treePoints(x, tls, size=2)
+add_treeIDs(x, tls, cex = 2, col='yellow')
+
+# classify stem points
+tls = stemPoints(tls, stm.hough())
+add_stemPoints(x, tls, color='red', size=4)
+
+# make the plot's inventory
+inv = tlsInventory(tls, d_method=shapeFit(shape='circle', algorithm = 'irls'))
+# add_tlsInventory(x, inv)
+
+# extract stem measures
+# seg = stemSegmentation(tls, sgt.ransac.circle(n = 20))
+# add_stemSegments(x, seg, color='white', fast=T)
+
+# plot everything once
+#x = tlsPlot(tls, map, inv, seg, fast=T)
+
+# check out only one tree
+#tlsPlot(tls, inv, seg, tree_id = 1)
+
